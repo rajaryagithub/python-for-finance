bc4a910901ec39f8154a4ba67003e7b8ad5fcff9
Rajesh Kumar <rajarya.java@gmail.com>
2020-01-01 16:51:43 +0100

    Fetching data from csv file and plotting

--- 01-Exercise.py
+++ 01-Exercise.py
@@ -6,10 +6,20 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2019, 12, 31)
+# Fetching data from yahoo
+# start = dt.datetime(2000, 1, 1)
+# end = dt.datetime(2019, 12, 31)
+#
+# df = web.DataReader('TSLA', 'yahoo', start, end)
+#print(df.head())
+#print(len(df))
 
-df = web.DataReader('TSLA', 'yahoo', start, end)
-print(df.head())
-print(df.tail())
-print(len(df))
+# Convert dataframe to csv
+# df.to_csv('tesla.csv')
+
+# Reading csv file from
+df = pd.read_csv('tesla.csv', parse_dates=True, index_col=0)
+print(df[['Open', 'High']].head())
+
+df['Adj Close'].plot()
+plt.show()
